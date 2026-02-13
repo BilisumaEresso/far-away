c42beebb25ac66e310075dc01cd029551a154d71
BilisumaEresso <bilisumaere@gmail.com>
2026-02-13 08:50:03 +0300

    add: list items:

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BaggageClaim, TreePalm } from "lucide-react";
 import { useState } from "react";
 
 function App() {
-  const [input, setInput] = useState({ name: "", quantity: 1 });
+  const [input, setInput] = useState({ name: "", quantity: 1,checked:false });
   const [item, setItem] = useState([]);
 
   function changeHandler(e) {
@@ -16,8 +16,9 @@ function App() {
   function submitHandler(e) {
     e.preventDefault();
     setItem((prev) => [...prev, input]);
-    console.log(item);
+    
   }
+  console.log(item);
   return (
     <div>
       <span className=" bg-amber-500 flex justify-center py-5 items-center gap-4">
@@ -27,13 +28,13 @@ function App() {
       </span>
       <form
         onSubmit={submitHandler}
-        className="bg-amber-600 p-5 md:flex justify-around px-2 md:gap-2 items-center"
+        className="bg-amber-600 p-5 sm:flex grid grid-cols-2 justify-around px-2 gap-2 items-center"
       >
-        <label className="" htmlFor="name">
+        <label className="col-span-2" htmlFor="name">
           What do you need for 😍 your trip?
         </label>
         <input
-          className="bg-amber-100 rounded-2xl p-2 max-w-30"
+          className="bg-amber-100 rounded-2xl p-2"
           type="number"
           name="quantity"
           onChange={changeHandler}
@@ -49,12 +50,18 @@ function App() {
         />
         <input
           type="submit"
-          className="bg-cyan-500 py-2 px-4 rounded-2xl uppercase cursor-pointer"
+          className="bg-cyan-500  py-2 px-4  rounded-2xl uppercase cursor-pointer"
           value="Add"
         />
       </form>
-      <div>
-        
+      <div className="grid grid-cols-3">
+        {item.map((i, idx) => (
+          <span className="text-amber-50 flex items-center py-3 gap-2" key={idx}>
+            <input type="checkbox" />
+            <p className="text-amber-50">{i.quantity}</p>
+            <p className="">{i.name}</p>
+          </span>
+        ))}
       </div>
     </div>
   );
